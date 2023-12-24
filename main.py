179cc4c88c6fb373f600b97084d6b13a53bb6fe8
pepeargentoo <dani_rc_22@hotmail.com>
2023-12-24 17:06:20 -0300

    smb

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from discover.arp_discover import arp_discover
+"""from discover.arp_discover import arp_discover
 from scanner.tcp import scanner_tcp
 
 ip = '192.168.1.11'
 mask = 24
 for host in arp_discover(ip_addr=ip, mask=mask):
     host_tmp = scanner_tcp(host)
-    print(host_tmp)
+    print(host_tmp)"""
+
+import os
+from protocols.smb import SambaClient
+
+client = SambaClient(server="10.129.105.31")
+try:
+    client.connect()
+    files = client.list_shares()
+    print("Files in the share:", files)
+
+
+finally:
+    client.close()
